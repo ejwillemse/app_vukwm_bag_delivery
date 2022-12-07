5927b276e4a71cb35ee01a1ab4f867086d6f821b
ejwillemse <ejwillemse@gmail.com>
2022-12-07 15:45:20 +0000

    feat: :sparkles: Added excel file upload feature
    #1

--- main_app.py
+++ main_app.py
@@ -1,4 +1,14 @@
 import streamlit as st
-import streamlit.components.v1 as components
+import pandas as pd
 
-st.write("Hello world!")
+st.write("VUKM Bag delivery")
+
+
+def upload_file():
+    uploaded_file = st.file_uploader("Choose an excel file to upload.")
+    if uploaded_file is not None:
+        df1 = pd.read_excel(uploaded_file)
+        st.text(df1)
+
+
+upload_file()
