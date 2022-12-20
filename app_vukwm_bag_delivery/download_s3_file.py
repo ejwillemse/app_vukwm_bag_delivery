3ede5433ef5adfbc65b0205cf57bac99bd862fdd
ejwillemse <ejwillemse@gmail.com>
2022-12-20 22:59:54 +0000

    feat: :sparkles: s3 bucket download file option added

---
+++ app_vukwm_bag_delivery/download_s3_file.py
@@ -0,0 +1,110 @@
+import json
+import logging
+import os
+from typing import Dict, List
+
+import boto3
+import pandas as pd
+import toml
+
+
+def get_s3_bucket_session(s3_cred: Dict[str, str], bucket: str):
+    aws_access_key_id = s3_cred["aws_access_key_id"]
+    aws_secret_access_key = s3_cred["aws_secret_access_key"]
+    session = boto3.Session(
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+    )
+    s3 = session.resource("s3")
+    my_bucket = s3.Bucket(bucket)
+    return my_bucket
+
+
+def download_file(my_bucket, s3_path, local_path):
+    if not os.path.isdir(local_path[: local_path.rfind("/") + 1]):
+        os.makedirs(local_path[: local_path.rfind("/")])
+    key = s3_path
+    upload = local_path
+    if os.path.isfile(local_path):
+        logging.info(f"File {key} already downloaded to {upload}")
+    else:
+        try:
+            my_bucket.download_file(key, upload)
+        except:
+            logging.error(f"Could not download {key} to {upload}")
+            raise ValueError("failure")
+
+
+def get_latest_bucket_files(my_bucket, prefix: str) -> pd.DataFrame:
+    existing_s3_files = my_bucket.objects.filter(Prefix=prefix)
+    pulled_results = []
+    for key in existing_s3_files:
+        pulled_results.append(
+            {"filename": key.key, "size": key.size, "last_modified": key.last_modified}
+        )
+    file_info = pd.DataFrame(pulled_results)
+    return file_info
+
+
+def download_return_latest_file(my_bucket, file_info, driver):
+    latest_file = file_info.sort_values(["last_modified"], ascending=False).iloc[0][
+        "filename"
+    ]
+    download_file(my_bucket, latest_file, "data/" + latest_file)
+    return driver("data/" + latest_file)
+
+
+def read_json_file(file_name: str):
+    """Readd single test file"""
+    with open(file_name, mode="r", encoding="utf-8") as file:
+        return json.load(file)
+
+
+def return_routing_files(
+    bucket,
+    s3_cred,
+    excel_prefix_path,
+    geocoded_prefix_path,
+    unassgined_stops_prefix_path,
+):
+    my_bucket = get_s3_bucket_session(s3_cred, bucket)
+
+    excel_files = get_latest_bucket_files(my_bucket, excel_prefix_path)
+    latest_excel_file = download_return_latest_file(
+        my_bucket, excel_files, pd.read_excel
+    )
+
+    geo_files = get_latest_bucket_files(my_bucket, geocoded_prefix_path)
+    latest_geo_file = download_return_latest_file(my_bucket, geo_files, pd.read_csv)
+
+    unassigned_stops_files = get_latest_bucket_files(
+        my_bucket, unassgined_stops_prefix_path
+    )
+    latest_unassigned_stops_file = download_return_latest_file(
+        my_bucket, unassigned_stops_files, read_json_file
+    )
+    return latest_excel_file, latest_geo_file, latest_unassigned_stops_file
+
+
+if __name__ == "__main__":
+    import toml
+
+    bucket = "veolia-uk-platfrom"
+    secrets = toml.load(".streamlit/secrets.toml")
+    bucket = secrets["bucket"]
+    s3_cred = secrets["dev_s3"]
+    input_paths = secrets["s3_input_paths"]
+    (
+        latest_excel_file,
+        latest_geo_file,
+        latest_unassigned_stops_file,
+    ) = return_routing_files(
+        bucket,
+        s3_cred,
+        input_paths["raw_user_input"],
+        input_paths["geocoded_input"],
+        input_paths["unassigned_stops_input"],
+    )
+    # print(latest_excel_file)
+    # print(latest_geo_file)
+    # print(latest_unassigned_stops_file)
--- routing_app.py
+++ routing_app.py
@@ -1,18 +1,15 @@
 import pandas as pd
 import streamlit as st
+
 import app_vukwm_bag_delivery.aggregates as aggregates
-import streamlit as st
-import pandas as pd
-from app_vukwm_bag_delivery.google_geocode import geocode_addresses
-import app_vukwm_bag_delivery.aggregates as aggregates
-from app_vukwm_bag_delivery.osrm_tsp import sequence_routes
 from app_vukwm_bag_delivery.download_to_excel import to_excel
+from app_vukwm_bag_delivery.google_geocode import geocode_addresses
+from app_vukwm_bag_delivery.osrm_tsp import sequence_routes
 from check_password import check_password
 from generate_routes import start_routing
-
-from upload_file_geocode import upload_and_geocode_file
-from select_vehicles import select_vehicles
 from routing_job_selection import stop_data_summary
+from select_vehicles import select_vehicles
+from upload_file_geocode import upload_and_geocode_file
 
 st.set_page_config(
     layout="wide",
