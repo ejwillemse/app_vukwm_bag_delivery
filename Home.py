77608ad3bdbf2f317d155ea46eb2f4aaca7df557
ejwillemse <ejwillemse@gmail.com>
2023-01-09 01:00:59 +0000

    refactor: :recycle: changed home view to page home view

--- Home.py
+++ Home.py
@@ -1,10 +1,12 @@
-import pandas as pd
 import streamlit as st
 
 import app_vukwm_bag_delivery.presenters.load_input_data as load_input_data
 import app_vukwm_bag_delivery.views.side_bar_progress as side_bar_progress
 from app_vukwm_bag_delivery.presenters.check_password import check_password
-from app_vukwm_bag_delivery.views.Home_view import set_page_config, view_instructions
+from app_vukwm_bag_delivery.views.page_view_Home import (
+    set_page_config,
+    view_instructions,
+)
 from app_vukwm_bag_delivery.views.return_session_status import return_full_status
 
 set_page_config()
@@ -17,7 +19,7 @@ side_bar_status = side_bar_progress.view_sidebar()
 view_instructions()
 
 if "stop_data" not in st.session_state:
-    with st.spinner(f"Initiating session and loading data..."):
+    with st.spinner("Initiating session and loading data..."):
         load_input_data.load_data()
 
 st.markdown(return_full_status())
