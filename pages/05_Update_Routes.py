f7bf444947c890fd8e941c1a9eff2d11bfd9d4e1
ejwillemse <ejwillemse@gmail.com>
2023-01-15 20:13:35 +0000

    refactor: :recycle: added update route empty page

--- pages/05_Update_Routes.py
+++ pages/05_Update_Routes.py
@@ -1,53 +1,69 @@
-import leafmap.foliumap as leafmap
+import pandas as pd
+import plotly.express as px
 import streamlit as st
+import streamlit.components.v1 as components
 
-from util_views.return_session_status import (
-    return_side_bar,
-    return_side_short,
+import app_vukwm_bag_delivery.generate_routes.presenters.extract_high_level_summary as extract_high_level_summary
+import app_vukwm_bag_delivery.util_views.return_session_status as return_session_status
+import app_vukwm_bag_delivery.util_views.side_bar_progress as side_bar_progress
+from app_vukwm_bag_delivery.util_presenters.check_password import check_password
+from app_vukwm_bag_delivery.view_routes.generate_route_display import (
+    return_assigned_stops_display,
 )
-from presenters.utils.check_password import check_password
+from app_vukwm_bag_delivery.view_routes.generate_route_gant import return_gant
+from app_vukwm_bag_delivery.view_routes.generate_route_map import return_map
+
+
+def set_page_config():
+    st.set_page_config(
+        layout="wide",
+        page_title="Update routes",
+        initial_sidebar_state="expanded",
+    )
+    st.title("Update routes")
+    st.subheader("Comming soon...")
+
 
 if not check_password():
     st.warning("Please log-in to continue.")
     st.stop()  # App won't run anything after this line
 
-st.title("Edit routes routes")
 
-st.sidebar.header("Session status")
-status_text = st.sidebar.empty()
-status_text.markdown(return_side_short())
+def view_instructions():
+    with st.expander("Instructions"):
+        st.markdown(
+            """
+        Perform the following steps to edit vehicle information and select the vehicles to be routed. If no vehicles are selected, it is assumed that the entire fleet is available for routing.
 
-with st.expander("Instructions"):
-    st.markdown(
+        * Step 1: Inspect the vehicle information in the table.
+        * Step 2: Edit the vehicle informaiton where required.
+        * Step 3: Select active vehicles by clicking on the boxes next to the vehicle ID.
+        * Step 4: Click on "Update" to load the vehicles.
         """
-    Perform the following steps to edit vehicle information and select the vehicles to be routed. If no vehicles are selected, it is assumed that the entire fleet is available for routing.
-
-    * Step 1: Inspect the vehicle information in the table.
-    * Step 2: Edit the vehicle informaiton where required.
-    * Step 3: Select active vehicles by clicking on the boxes next to the vehicle ID.
-    * Step 4: Click on "Update" to load the vehicles.
-    """
-    )
-st.write("To be released soon...")
-
-# if "routes" not in st.session_state:
-#     st.warning(
-#         "Routes have not yet been generated. Please go to the 'Generate Routes' page."
-#     )
-#     st.stop()
+        )
 
 
-st.title("Home")
+def check_previous_steps_completed():
+    stop = False
+    if not return_session_status.check_intermediate_unassigned_jobs_loaded():
+        st.warning("Job data not loaded during session. Please go back to `Home` page")
+        stop = True  # App won't run anything after this line
+    if not return_session_status.check_intermediate_unassigned_fleet_loaded():
+        st.warning(
+            "Vehicles have not yet been configured and selected. Please go to the `Select Vehicles` page."
+        )
+        stop = True
+    if not return_session_status.check_route_generation_completed():
+        st.warning(
+            "Routes have not yet been generated. Please go to the `Generate Routes` page."
+        )
+        stop = True  # App won't run anything after this line
+    if stop:
+        st.stop()
 
-st.markdown(
-    """
-A [streamlit](https://streamlit.io) app template for geospatial applications based on [streamlit-option-menu](https://github.com/victoryhb/streamlit-option-menu). 
-To create a direct link to a pre-selected menu, add `?page=<app name>` to the URL, e.g., `?page=upload`.
-https://share.streamlit.io/giswqs/streamlit-template?page=upload
-"""
-)
 
-m = leafmap.Map(locate_control=True)
-m.add_basemap("ROADMAP")
-m.to_streamlit(height=700)
-
+set_page_config()
+side_bar_status = side_bar_progress.view_sidebar()
+check_previous_steps_completed()
+view_instructions()
+side_bar_progress.update_side_bar(side_bar_status)
