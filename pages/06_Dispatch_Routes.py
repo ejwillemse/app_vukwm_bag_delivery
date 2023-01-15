685d77074b034dcff16cac806fec1924f8d177d3
ejwillemse <ejwillemse@gmail.com>
2023-01-15 23:17:12 +0000

    test: :monocle_face: basic logic for sheet completion added
    #37

--- pages/06_Dispatch_Routes.py
+++ pages/06_Dispatch_Routes.py
@@ -1,6 +1,7 @@
 import datetime
 from io import BytesIO
 
+import numpy as np
 import pandas as pd
 import streamlit as st
 
@@ -117,7 +118,8 @@ def create_formatted_assigned_jobs():
         **{
             "Visit sequence": (assigned_jobs["Visit sequence"] + 1)
             .fillna(0)
-            .astype(int)
+            .astype(int),
+            "Vehicle Id": assigned_jobs["Vehicle Id"].fillna("Unassigned"),
         }
     )
     st.session_state.data_07_reporting["assigned_jobs_download"] = assigned_jobs.copy()
@@ -162,4 +164,241 @@ show_assigned_routes()
 download_results()
 side_bar_progress.update_side_bar(side_bar_status)
 
+assigned_jobs = st.session_state.data_07_reporting["assigned_jobs_download"].copy()
 
+st.write(assigned_jobs)
+
+date = datetime.datetime.today().strftime("%d/%m/%Y %H:%M:%S")
+st.write(date)
+
+route_totals = (
+    assigned_jobs.assign(
+        **{
+            "Vehicle Id": assigned_jobs["Vehicle Id"].fillna("Unassigned"),
+            "Vehicle type": assigned_jobs["Vehicle type"].fillna(""),
+        }
+    )
+    .groupby(["Vehicle Id", "Vehicle type"])
+    .agg(
+        **{
+            "total_stops": ("Site Bk", "nunique"),
+            "total_stops2": ("Visit sequence", "max"),
+        }
+    )
+).reset_index()
+st.write(route_totals)
+
+
+route_product_totals = (
+    assigned_jobs.assign(
+        **{"Vehicle Id": assigned_jobs["Vehicle Id"].fillna("Unassigned")}
+    )
+    .groupby(["Vehicle Id", "Product Name"])
+    .agg(**{"product_total": ("Quantity", "sum")})
+).reset_index()
+st.write(route_product_totals)
+
+
+def calc_page_number(stop_i):
+    return int(np.floor((stop_i - 4) / 5) + 2)
+
+
+def page1_formula(stop_i, job_i, page, start_row):
+    return 18 + start_row + job_i + (stop_i - 1) * 7
+
+
+def page2_formula(stop_i, job_i, page, start_row):
+    return 44 + start_row + job_i + (stop_i - 4) * 7 + 6 * (page - 2)
+
+
+def create_job_id_cells(stop_i, job_i, page, row_formula):
+    start_row = 2
+    return f"A:{row_formula(stop_i, job_i, page, start_row)}"
+
+
+def create_product_cells(stop_i, job_i, page, row_formula):
+    start_row = 2
+    return f"D:{row_formula(stop_i, job_i, page, start_row)}"
+
+
+def create_product_quantity_cells(stop_i, job_i, page, row_formula):
+    start_row = 2
+    return f"E:{row_formula(stop_i, job_i, page, start_row)}"
+
+
+def get_row_number(stop_i, job_i, page, start_row):
+    if page == 0:
+        return page1_formula(stop_i, job_i, page, start_row)
+    else:
+        return page2_formula(stop_i, job_i, page, start_row)
+
+
+def create_job_cells(site_jobs, stop_i, page_i):
+    cell_values = []
+    if site_jobs.shape[0] > 6:
+        st.warning(
+            f"Site {site_jobs['Site Bk'].unique()[0]} has {site_jobs.shape[0]} jobs. Only six jobs can fit in the route sheet.."
+        )
+        st.write(site_jobs)
+    for job_i, job_row in (
+        site_jobs[["Ticket No", "Product Name", "Quantity"]].reset_index().iterrows()
+    ):
+        if job_i >= 6:
+            break
+        if page_i == 1:
+            row_formula = page1_formula
+        else:
+            row_formula = page2_formula
+        cell_values.append(
+            {
+                create_job_id_cells(stop_i, job_i, page_i, row_formula): job_row[
+                    "Ticket No"
+                ]
+            }
+        )
+        cell_values.append(
+            {
+                create_product_cells(stop_i, job_i, page_i, row_formula): job_row[
+                    "Product Name"
+                ]
+            }
+        )
+        cell_values.append(
+            {
+                create_product_quantity_cells(
+                    stop_i, job_i, page_i, row_formula
+                ): job_row["Quantity"]
+            }
+        )
+    return cell_values
+
+
+def group_types(df):
+    grouped_tickets = (
+        df.groupby(["Product Name"])
+        .agg(**{"Ticket No": ("Ticket No", "unique"), "Quantity": ("Quantity", "sum")})
+        .reset_index()
+    )
+    grouped_tickets = grouped_tickets.assign(
+        **{"Ticket No": grouped_tickets["Ticket No"].apply(lambda x: ";".join(x))}
+    )
+    return grouped_tickets
+
+
+def get_job_row_info(assigned_route_jobs):
+    sites = assigned_route_jobs["Site Bk"].unique()
+    job_cells = []
+    for route_site in sites:
+        site_jobs = assigned_route_jobs.loc[
+            assigned_route_jobs["Site Bk"] == route_site
+        ]
+        stop_i = site_jobs["stop_number"].unique()[0]
+        page_i = calc_page_number(stop_i)
+        job_cells += create_job_cells(group_types(site_jobs), stop_i, page_i)
+    return job_cells
+
+
+def return_route_info(assigned_route_jobs):
+    assigned_route_jobs = assigned_route_jobs.assign(
+        sort_site=assigned_route_jobs["Site Name"].str.lower().str.strip()
+    )
+    assigned_route_jobs = assigned_route_jobs.sort_values(
+        ["Visit sequence", "sort_site", "Site Bk"]
+    ).assign(
+        stop_number=assigned_route_jobs.groupby(
+            ["Visit sequence", "sort_site", "Site Bk"]
+        ).ngroup()
+        + 1
+    )
+    return assigned_route_jobs
+
+
+def create_stop_cells(stop_info):
+    stop_cells = []
+    for _, stop_row in stop_info.iterrows():
+        stop_i = stop_row["stop_number"]
+        page_i = calc_page_number(stop_i)
+        stop_cells += [
+            {
+                f"job_number": stop_i,
+                f"B:{get_row_number(stop_i, 0, page_i, start_row=1)}": stop_row[
+                    "Site Name"
+                ],
+                f"B:{get_row_number(stop_i, 0, page_i, start_row=3)}": stop_row[
+                    "Site Address"
+                ],
+                f"B:{get_row_number(stop_i, 0, page_i, start_row=4)}": stop_row[
+                    "Site Post Town"
+                ],
+                f"B:{get_row_number(stop_i, 0, page_i, start_row=5)}": stop_row[
+                    "Site Post Code"
+                ],
+                f"B:{get_row_number(stop_i, 0, page_i, start_row=6)}": stop_row[
+                    "Transport Area Code"
+                ],
+                f"F:{get_row_number(stop_i, 0, page_i, start_row=1)}": stop_row[
+                    "Notes"
+                ],
+                f"G:{get_row_number(stop_i, 0, page_i, start_row=6)}": stop_row[
+                    "Visit sequence"
+                ],
+                f"G:{get_row_number(stop_i, 0, page_i, start_row=7)}": stop_row[
+                    "Arrival time"
+                ],
+            }
+        ]
+    return stop_cells
+
+
+def return_stop_info(assigned_jobs):
+    stop_info = (
+        assigned_jobs[
+            [
+                "Site Name",
+                "Notes",
+                "Arrival time",
+                "Site Address1",
+                "Site Address2",
+                "Site Address3",
+                "Site Post Town",
+                "Site Post Code",
+                "Transport Area Code",
+                "Visit sequence",
+                "stop_number",
+            ]
+        ]
+        .drop_duplicates()
+        .sort_values(["Notes"])
+    )
+    stop_info = (
+        stop_info.assign(
+            **{
+                "Site Address": stop_info[
+                    ["Site Address1", "Site Address2", "Site Address3"]
+                ].apply(
+                    lambda x: ", ".join([y for y in x if y is not pd.np.nan]), axis=1
+                )
+            }
+        )
+        .drop(columns=["Site Address1", "Site Address2", "Site Address3"])
+        .fillna("")
+    )
+    return stop_info
+
+
+def generate_sheet_cells():
+    assigned_jobs = st.session_state.data_07_reporting["assigned_jobs_download"].copy()
+    route_ids = assigned_jobs["Vehicle Id"].unique()
+    sheet_cells = {}
+    for route_id in route_ids:
+        assigned_route_jobs = assigned_jobs.loc[assigned_jobs["Vehicle Id"] == route_id]
+        assigned_route_jobs = return_route_info(assigned_route_jobs)
+        stop_info = return_stop_info(assigned_route_jobs)
+        stop_cells = create_stop_cells(stop_info)
+        job_cells = get_job_row_info(assigned_route_jobs)
+        sheet_cells[route_id] = {"job_cells": job_cells, "stop_cells": stop_cells}
+    return sheet_cells
+
+
+sheet_cells = generate_sheet_cells()
+st.write(sheet_cells)
